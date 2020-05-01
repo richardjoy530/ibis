665c49a1fb3c795f241fb5c9464226068fe63c78
Richard Joy <richardjoy530@gmail.com>
2020-05-02 00:37:07 +0530

    Initial Commit

--- lib/main.dart
+++ lib/main.dart
@@ -90,6 +90,7 @@ class _MyAppState extends State<MyApp> {
                               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                               children: <Widget>[
                                 AnimatedContainer(
+
                                   height: buttonRadius,
                                   width: buttonRadius,
                                   child: ClayContainer(
