43f7aa72fa9c290b493cfac9b107aa25c5461979
Richard Joy <richardjoy530@gmail.com>
2020-05-03 00:59:40 +0530

    Beautifully Animated Power Button

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
-import 'package:clay_containers/clay_containers.dart';
+import 'package:flare_flutter/flare_actor.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_blue/flutter_blue.dart';
 
 void main() => runApp(Main());
 
@@ -22,19 +21,10 @@ class MyApp extends StatefulWidget {
   _MyAppState createState() => _MyAppState();
 }
 
-class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
+class _MyAppState extends State<MyApp> {
   bool power = false;
-  double buttonRadius = 100;
-  Animation animation;
-  Animation colorAnimation;
-  AnimationController animationController;
-  bool mReverse;
   @override
   void initState() {
-    animationController =
-        AnimationController(vsync: this, duration: Duration(milliseconds: 300));
-    animation = Tween<double>(begin: 0, end: 1).animate(animationController);
-    //colorAnimation = ColorTween();
     super.initState();
   }
 
@@ -86,50 +76,19 @@ class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
                 ),
                 Padding(
                   padding: const EdgeInsets.all(30.0),
-                  child: ClayContainer(
-                    spread: 5,
-                    surfaceColor: Color(0xFF4dc9cf),
-                    color: Color(0xffb9dfe6),
-                    borderRadius: 50,
-                    curveType: CurveType.convex,
-                    height: 100,
-                    width: 100,
-                    child: AnimatedBuilder(
-//                        child: Container(
-//                          child: IconButton(
-//                              icon: AnimatedIcon(
-//                                icon: AnimatedIcons.play_pause,
-//                                progress: animation,
-//                              ),
-//                              onPressed: () {
-//                                if (mReverse == true) {
-//                                  animationController.forward();
-//                                  mReverse = false;
-//                                } else {
-//                                  animationController.reverse();
-//                                  mReverse = true;
-//                                }
-//                              }),
-//                        ),
-                      animation: animation,
-                      builder: (BuildContext context, Widget child) {
-                        return Container(
-                          child: IconButton(
-                              icon: AnimatedIcon(
-                                icon: AnimatedIcons.play_pause,
-                                progress: animation,
-                              ),
-                              onPressed: () {
-                                if (mReverse == true) {
-                                  animationController.forward();
-                                  mReverse = false;
-                                } else {
-                                  animationController.reverse();
-                                  mReverse = true;
-                                }
-                              }),
-                        );
-                      },
+                  child: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        print('tap');
+                        power = !power;
+                      });
+                    },
+                    child: Container(
+                      height: 100,
+                      width: 100,
+                      child: FlareActor('assets/powerButton.flr',
+                          //alignment: Alignment.center,
+                          animation: power == true ? "on" : "off"),
                     ),
                   ),
                 )
@@ -148,47 +107,10 @@ class BlueToothScreen extends StatefulWidget {
 }
 
 class _BlueToothScreenState extends State<BlueToothScreen> {
-  FlutterBlue flutterBlue = FlutterBlue.instance;
-  BluetoothDevice bluetoothDevice;
-  void test() {
-    flutterBlue.startScan(
-        timeout: Duration(seconds: 4), scanMode: ScanMode.balanced);
-    print('In function');
-// Listen to scan results
-    flutterBlue.scanResults.listen((results) {
-      // do something with scan results
-      for (ScanResult r in results) {
-        print(
-            '${r.device.name} found! rssi: ${r.rssi} id: ${r.device.id.toString()} id.id: ${r.device.id.id}');
-        bluetoothDevice = r.device;
-      }
-    });
-
-// Stop scanning
-    flutterBlue.stopScan();
-  }
-
-  void connect() {
-    bluetoothDevice.connect();
-    print('Connection initiated');
-  }
-
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Scaffold(
-        body: Container(
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: <Widget>[
-                IconButton(icon: Icon(Icons.bluetooth), onPressed: test),
-                IconButton(icon: Icon(Icons.touch_app), onPressed: connect)
-              ],
-            ),
-          ),
-        ),
-      ),
+      child: Scaffold(),
     );
   }
 }
