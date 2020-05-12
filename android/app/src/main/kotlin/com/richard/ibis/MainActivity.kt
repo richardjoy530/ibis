eb74e983cc3f9e7b676b9a56e82ab67d12167e24
Richard Joy <richardjoy530@gmail.com>
2020-05-12 19:26:42 +0530

    Saved height and name in memory

--- android/app/src/main/kotlin/com/richard/ibis/MainActivity.kt
+++ android/app/src/main/kotlin/com/richard/ibis/MainActivity.kt
@@ -1,28 +1,21 @@
 package com.richard.ibis
-import android.content.Context
-import android.net.wifi.WifiManager
 import android.os.Build
 import android.os.Bundle
-import android.os.Handler
 import androidx.annotation.NonNull
 import androidx.annotation.RequiresApi
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugins.GeneratedPluginRegistrant
-import java.util.*
 
 class MainActivity: FlutterActivity() {
     private val channel = "com.richard.ibis/ibis"
-    private lateinit var wifiManager: WifiManager
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        wifiManager = this.applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
         MethodChannel(flutterEngine?.dartExecutor,channel).setMethodCallHandler{ MethodCall, result->
             if (MethodCall.method=="test"){
-                //wifiManager.isWifiEnabled = false
-                result.success("test")
+                result.success("Test")
             }
         }
     }
