a3453e56f1f2dbc40cd0ceef4ed08cf1909738dc
Richard Joy <richardjoy530@gmail.com>
2020-05-23 14:35:22 +0530

    Loading indefinite

--- lib/loding.dart
+++ lib/loding.dart
@@ -10,67 +10,63 @@ class Loding extends StatefulWidget {
 }
 
 class _LodingState extends State<Loding> {
-  Timer time,Time;
-  double per=0;
+  Timer time, timer;
+  double per = 0;
   @override
   void initState() {
-    // TODO: implement initState
-    Time=Timer.periodic(Duration(milliseconds: 100), (timer) {
+    timer = Timer.periodic(Duration(milliseconds: 100), (timer) {
       setState(() {
-        if(per<1.0) {
-          if(per>0.9)
-            {
-              per=1.0;
-            }
-          else {
+        if (per < 1.0) {
+          if (per > 0.9) {
+            per = 1.0;
+          } else {
             per += 0.05;
           }
         }
       });
-
     });
     redirect();
     super.initState();
   }
-  void redirect()
-  {
-    time=Timer.periodic(Duration(seconds: 2), (timer) {
+
+  void redirect() {
+    time = Timer.periodic(Duration(seconds: 2), (timer) {
       Navigator.pushReplacement(context,
       MaterialPageRoute(builder: (context)=>FrontPage())
       );
     });
   }
+
   @override
   void dispose() {
-    // TODO: implement dispose
     print('Loding page disposed');
-    Time.cancel();
+    timer.cancel();
     time.cancel();
     super.dispose();
   }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Stack(
-          children: <Widget>[
-              Align(
-                alignment: Alignment.center,
-                child: Text('Loading',style: TextStyle(fontSize: 60),),
-              ),
-              Positioned(
-                bottom: 50.0,
-                left: MediaQuery.of(context).size.width/3,
-                child: LinearPercentIndicator(
-                  lineHeight: 6.0,
-                  width: 150,
-                  percent:per ,
-                  progressColor: Colors.blue,
-                ),
-              )
-            ],
-        )
-      ),
+          body: Stack(
+        children: <Widget>[
+          Align(
+            alignment: Alignment.center,
+            child: Text(
+              'Loading',
+              style: TextStyle(fontSize: 60),
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              padding: EdgeInsets.all(50),
+              child: LinearProgressIndicator(),
+            ),
+          )
+        ],
+      )),
     );
   }
 }
