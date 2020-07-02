f3816510103e63a984b24f3bdabb98bf9544e0fd
josephtjoshy <josephtjoshy9@gmail.com>
2020-07-02 15:45:07 +0530

    fl graph

--- lib/calender.dart
+++ lib/calender.dart
@@ -71,6 +71,7 @@ class _CalenderPageState extends State<CalenderPage> {
           //_buildButtons(),
           //const SizedBox(height: 8.0),
           Expanded(
+
               child: _buildEventList()
           ),
         ],
@@ -113,7 +114,102 @@ class _CalenderPageState extends State<CalenderPage> {
       height:MediaQuery.of(context).size.height/2,
       width: MediaQuery.of(context).size.width/1,
       child: BarChart(
+      BarChartData(
+      alignment: BarChartAlignment.spaceAround,
+      maxY: 20,
+      barTouchData: BarTouchData(
+        enabled: false,
+        touchTooltipData: BarTouchTooltipData(
+          tooltipBgColor: Colors.transparent,
+          tooltipPadding: const EdgeInsets.all(0),
+          tooltipBottomMargin: 8,
+          getTooltipItem: (
+              BarChartGroupData group,
+              int groupIndex,
+              BarChartRodData rod,
+              int rodIndex,
+              ) {
+            return BarTooltipItem(
+              rod.y.round().toString(),
+              TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+              ),
+            );
+          },
+        ),
+      ),
 
+        titlesData: FlTitlesData(
+          show: true,
+          bottomTitles: SideTitles(
+            showTitles: true,
+            textStyle: TextStyle(
+                color: const Color(0xff7589a2), fontWeight: FontWeight.bold, fontSize: 14),
+            margin: 20,
+            getTitles: (double value) {
+              switch (value.toInt()) {
+                case 0:
+                  return '8AM';
+                case 1:
+                  return '11AM';
+                case 2:
+                  return '2PM';
+                case 3:
+                  return '5PM';
+                case 4:
+                  return '8PM';
+                case 5:
+                  return '11PM';
+                case 6:
+                  return '2AM';
+                case  7:
+                  return '5AM';
+                default:
+                  return '';
+              }
+            },
+          ),
+          leftTitles: SideTitles(showTitles: false),
+        ),
+        borderData: FlBorderData(
+          show: false,
+        ),
+        barGroups: [
+          BarChartGroupData(
+              x: 0,
+              barRods: [BarChartRodData(y: 8, color: Colors.lightBlueAccent)],
+              showingTooltipIndicators: [0]),
+          BarChartGroupData(
+              x: 1,
+              barRods: [BarChartRodData(y: 10, color: Colors.lightBlueAccent)],
+              showingTooltipIndicators: [0]),
+          BarChartGroupData(
+              x: 2,
+              barRods: [BarChartRodData(y: 14, color: Colors.lightBlueAccent)],
+              showingTooltipIndicators: [0]),
+          BarChartGroupData(
+              x: 3,
+              barRods: [BarChartRodData(y: 15, color: Colors.lightBlueAccent)],
+              showingTooltipIndicators: [0]),
+          BarChartGroupData(
+              x: 4,
+              barRods: [BarChartRodData(y: 13, color: Colors.lightBlueAccent)],
+              showingTooltipIndicators: [0]),
+          BarChartGroupData(
+              x: 5,
+              barRods: [BarChartRodData(y: 20, color: Colors.lightBlueAccent)],
+              showingTooltipIndicators: [0]),
+          BarChartGroupData(
+              x: 6,
+              barRods: [BarChartRodData(y: 20, color: Colors.lightBlueAccent)],
+              showingTooltipIndicators: [0]),
+          BarChartGroupData(
+              x: 7,
+              barRods: [BarChartRodData(y: 20, color: Colors.lightBlueAccent)],
+              showingTooltipIndicators: [0]),
+        ],
+      ),
       ),
     );
   }
