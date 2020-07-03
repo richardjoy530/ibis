d5c9661a24d2e7140fb92f989998e3a54d2a7997
josephtjoshy <josephtjoshy9@gmail.com>
2020-07-03 12:39:27 +0530

    edit chart

--- lib/calender.dart
+++ lib/calender.dart
@@ -72,102 +72,107 @@ class _CalenderPageState extends State<CalenderPage> {
           //_buildButtons(),
           //const SizedBox(height: 8.0),
           Expanded(
-            child: BarChart(
-              BarChartData(
-                alignment: BarChartAlignment.spaceAround,
-                maxY: 20,
-                barTouchData: BarTouchData(
-                  enabled: true,
-                  touchTooltipData: BarTouchTooltipData(
-                    tooltipBgColor: Colors.transparent,
-                    tooltipPadding: const EdgeInsets.all(0),
-                    tooltipBottomMargin: 8,
-                    getTooltipItem: (
-                        BarChartGroupData group,
-                        int groupIndex,
-                        BarChartRodData rod,
-                        int rodIndex,
-                        ) {
-                      return BarTooltipItem(
-                        rod.y.round().toString(),
-                        TextStyle(
-                          color: Colors.blueGrey,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      );
-                    },
+            child: Container(
+              height: MediaQuery.of(context).size.height/2,
+              width: MediaQuery.of(context).size.width/1,
+              padding: EdgeInsets.all(8.0),
+              child: BarChart(
+                BarChartData(
+                  alignment: BarChartAlignment.spaceAround,
+                  maxY: 20,
+                  barTouchData: BarTouchData(
+                    enabled: true,
+                    touchTooltipData: BarTouchTooltipData(
+                      tooltipBgColor: Colors.transparent,
+                      tooltipPadding: const EdgeInsets.all(0),
+                      tooltipBottomMargin: 8,
+                      getTooltipItem: (
+                          BarChartGroupData group,
+                          int groupIndex,
+                          BarChartRodData rod,
+                          int rodIndex,
+                          ) {
+                        return BarTooltipItem(
+                          rod.y.round().toString(),
+                          TextStyle(
+                            color: Colors.blueGrey,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        );
+                      },
+                    ),
                   ),
-                ),
-                titlesData: FlTitlesData(
-                  show: true,
-                  bottomTitles: SideTitles(
-                    showTitles: true,
-                    textStyle: TextStyle(
-                        color: const Color(0xff7589a2), fontWeight: FontWeight.bold, fontSize: 14),
-                    margin: 20,
-                    getTitles: (double value) {
-                      switch (value.toInt()) {
-                        case 0:
-                          return '12AM';
-                        case 1:
-                          return '3AM';
-                        case 2:
-                          return '6AM';
-                        case 3:
-                          return '9AM';
-                        case 4:
-                          return '12PM';
-                        case 5:
-                          return '3PM';
-                        case 6:
-                          return '6PM';
-                        case 7:
-                          return '9PM';
-                        default:
-                          return '';
-                      }
-                    },
+                  titlesData: FlTitlesData(
+                    show: true,
+                    bottomTitles: SideTitles(
+                      showTitles: true,
+                      textStyle: TextStyle(
+                          color: const Color(0xff7589a2), fontWeight: FontWeight.bold, fontSize: 14),
+                      margin: 20,
+                      getTitles: (double value) {
+                        switch (value.toInt()) {
+                          case 0:
+                            return '12AM';
+                          case 1:
+                            return '3AM';
+                          case 2:
+                            return '6AM';
+                          case 3:
+                            return '9AM';
+                          case 4:
+                            return '12PM';
+                          case 5:
+                            return '3PM';
+                          case 6:
+                            return '6PM';
+                          case 7:
+                            return '9PM';
+                          default:
+                            return '';
+                        }
+                      },
+                    ),
+                    leftTitles: SideTitles(showTitles: false),
                   ),
-                  leftTitles: SideTitles(showTitles: false),
-                ),
-                borderData: FlBorderData(
-                  show: false,
-                ),
-                barGroups: [
-                  BarChartGroupData(
-                      x: 0,
-                      barRods: [BarChartRodData(y: time12am, color: Colors.lightBlueAccent)],
-                      showingTooltipIndicators: [0]),
-                  BarChartGroupData(
-                      x: 1,
-                      barRods: [BarChartRodData(y: time3am, color: Colors.lightBlueAccent)],
-                      showingTooltipIndicators: [0]),
-                  BarChartGroupData(
-                      x: 2,
-                      barRods: [BarChartRodData(y: time6am, color: Colors.lightBlueAccent)],
-                      showingTooltipIndicators: [0]),
-                  BarChartGroupData(
-                      x: 3,
-                      barRods: [BarChartRodData(y: time9am, color: Colors.lightBlueAccent)],
-                      showingTooltipIndicators: [0]),
-                  BarChartGroupData(
-                      x: 4,
-                      barRods: [BarChartRodData(y: time12pm, color: Colors.lightBlueAccent)],
-                      showingTooltipIndicators: [0]),
-                  BarChartGroupData(
-                      x: 5,
-                      barRods: [BarChartRodData(y: time3pm, color: Colors.lightBlueAccent)],
-                      showingTooltipIndicators: [0]),
-                  BarChartGroupData(
-                      x: 6,
-                      barRods: [BarChartRodData(y: time6pm, color: Colors.lightBlueAccent)],
-                      showingTooltipIndicators: [0]),
-                  BarChartGroupData(
-                      x: 7,
-                      barRods: [BarChartRodData(y: time9pm, color: Colors.lightBlueAccent)],
-                      showingTooltipIndicators: [0]),
+                  borderData: FlBorderData(
+                    show: false,
+                  ),
+                  barGroups: [
+                    BarChartGroupData(
+                        x: 0,
+                        barRods: [BarChartRodData(y: time12am, color: Colors.lightBlueAccent)],
+                        showingTooltipIndicators: [0]),
+                    BarChartGroupData(
+                        x: 1,
+                        barRods: [BarChartRodData(y: time3am, color: Colors.lightBlueAccent)],
+                        showingTooltipIndicators: [0]),
+                    BarChartGroupData(
+                        x: 2,
+                        barRods: [BarChartRodData(y: time6am, color: Colors.lightBlueAccent)],
+                        showingTooltipIndicators: [0]),
+                    BarChartGroupData(
+                        x: 3,
+                        barRods: [BarChartRodData(y: time9am, color: Colors.lightBlueAccent)],
+                        showingTooltipIndicators: [0]),
+                    BarChartGroupData(
+                        x: 4,
+                        barRods: [BarChartRodData(y: time12pm, color: Colors.lightBlueAccent)],
+                        showingTooltipIndicators: [0]),
+                    BarChartGroupData(
+                        x: 5,
+                        barRods: [BarChartRodData(y: time3pm, color: Colors.lightBlueAccent)],
+                        showingTooltipIndicators: [0]),
+                    BarChartGroupData(
+                        x: 6,
+                        barRods: [BarChartRodData(y: time6pm, color: Colors.lightBlueAccent)],
+                        showingTooltipIndicators: [0]),
+                    BarChartGroupData(
+                        x: 7,
+                        barRods: [BarChartRodData(y: time9pm, color: Colors.lightBlueAccent)],
+                        showingTooltipIndicators: [0]),
 
-                ],
+                  ],
+                ),
               ),
             ),
 
