4fec1dce05f2ff35ced966c3f0f12b3abf1811a0
richardjoy530 <richardjoy530@users.noreply.github.com>
2020-08-20 09:22:59 +0530

    warning removeed

--- lib/main.dart
+++ lib/main.dart
@@ -7,8 +7,6 @@ import 'package:flare_flutter/flare_actor.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-import 'package:ibis/front_page.dart';
-import 'package:ibis/select_time.dart';
 import 'package:intl/intl.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:sleek_circular_slider/sleek_circular_slider.dart';
